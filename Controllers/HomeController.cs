84b192c1b58c7a87f7f317838c1f08ca29b5dfb6
Sami1984Careeria <sami.palomaki@hotmail.com>
2021-12-16 16:16:02 +0200

    päivitetty tietokanta-Northwind

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -10,21 +10,69 @@ namespace WebAppTietokantasovellus.Controllers
     {
         public ActionResult Index()
         {
+            if (Session["UserName"] == null)
+            {
+                ViewBag.LoggedStatus = "Out";
+                return RedirectToAction("login", "home");
+            }
+            else ViewBag.LoggedStatus = "In";
             return View();
         }
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            if (Session["UserName"] == null)
+            {
+                return RedirectToAction("login", "home");
+            }
+            else
+            {
+                ViewBag.Message = "Yhtiön Careeria tietoja.";
 
-            return View();
+                return View();
+            }
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Yhteystiedot.";
+            if (Session["UserName"] == null)
+            {
+                return RedirectToAction("login", "home");
+            }
+            else
+            {
+                ViewBag.Message = "Yhteystietoja.";
+                ViewBag.UserName = Session["UserName"];
+                return View();
+            }
+        }
 
-            return View();
+        [HttpPost]
+        public ActionResult Authorize(Logins LoginModel)
+        {
+            NorthwindEntities db = new NorthwindEntities();
+            //Haetaan käyttäjän/Loginin tiedot annetuilla tunnustiedoilla tietokannasta LINQ -kyselyllä
+            var LoggedUser = db.Logins.SingleOrDefault(x => x.UserName == LoginModel.UserName && x.PassWord == LoginModel.PassWord);
+            if (LoggedUser != null)
+            {
+                ViewBag.LoginMessage = "Successfull login";
+                ViewBag.LoggedStatus = "In";
+                Session["UserName"] = LoggedUser.UserName;  // Perustetaan user-name sessio
+                return RedirectToAction("Index", "Home"); //Tässä määritellään mihin onnistunut kirjautuminen johtaa --> Home/Index
+            }
+            else
+            {
+                ViewBag.LoginMessage = "Login unsuccessfull";
+                ViewBag.LoggedStatus = "Out";
+                LoginModel.LoginErrorMessage = "Tuntematon käyttäjätunnus tai salasana.";
+                return View("Login", LoginModel);
+            }
+        }
+        public ActionResult LogOut()
+        {
+            Session.Abandon();
+            ViewBag.LoggedStatus = "Out";
+            return RedirectToAction("Index", "Home"); //Uloskirjautumisen jälkeen pääsivulle
         }
     }
 }
---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAppTietokantasovellus.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            if (Session["UserName"] == null)
+            {
+                return RedirectToAction("login", "home");
+            }
+            else
+            {
+                NorthwindEntities db = new NorthwindEntities();
+                List<Products> model = db.Products.ToList();
+                db.Dispose();
+                return View(model);
+            }
+        }
+    }
+}
